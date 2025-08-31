233df676b11e72ad816702cd1983a7ba9d7f8c7b
Rachit Kumar Verma <rachitverma1122@gmail.com>
2025-08-31 23:08:45 +0530

    removed dotconfig (into gentoo)

--- dotconfig/fish/functions/y.fish
+++
@@ -1,8 +0,0 @@
-function y
-    set -l tmp (mktemp -t "yazi-cwd.XXXXX")
-    command yazi $argv --cwd-file="$tmp"
-    if set cwd (cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-        builtin cd -- "$cwd"
-    end
-    rm -f -- "$tmp"
-end
