6e6a0274ed25691928dcdc5325bb5c22423670ae
Rachit Kumar Verma <rachitverma1122@gmail.com>
2025-09-05 01:33:34 +0530

    add: jothananharty/gruvbox-material-icon-theme to vscodium
    TODO: package it and issue a PR.

--- home-manager/Editors/vscodium.nix
+++ home-manager/Editors/vscodium.nix
@@ -1,4 +1,14 @@
 { pkgs, ... }:
+let
+  jonathanharty.gruvbox-material-icon-theme = pkgs.vscode-utils.buildVscodeMarketplaceExtension {
+    mktplcRef = {
+      name = "gruvbox-material-icon-theme";
+      publisher = "JonathanHarty";
+      version = "1.1.5";
+      hash = "sha256-86UWUuWKT6adx4hw4OJw3cSZxWZKLH4uLTO+Ssg75gY=";
+    };
+  };
+in
 {
   home.packages = with pkgs; [
     clang-tools
@@ -25,6 +35,7 @@
             foxundermoon.shell-format
             llvm-vs-code-extensions.vscode-clangd
             jnoortheen.nix-ide
+            jonathanharty.gruvbox-material-icon-theme
             oderwat.indent-rainbow
             redhat.java
             tamasfe.even-better-toml
@@ -36,7 +47,7 @@
             "editor.lineNumbers" = "relative";
             "editor.formatOnSave" = true;
             "files.trimTrailingWhitespace" = true;
-            "workbench.iconTheme" = "catppuccin-mocha";
+            "workbench.iconTheme" = "gruvbox-material-icon-theme";
             "redhat.telemetry.enabled" = true;
             "nix.enableLanguageServer" = true;
             "nix.serverPath" = "nixd";
