969b78cba48d2c2b741a6ccad21a09c12ec53c17
Rachit Kumar Verma <rachitverma1122@gmail.com>
2025-09-27 12:55:07 +0530

    add: rainbow_delimiters + indent-blankline integration (not working)

--- home-manager/Editors/neovim/plugins/visuals.nix
+++ home-manager/Editors/neovim/plugins/visuals.nix
@@ -1,5 +1,50 @@
+{ lib, nvf, ... }:
+let
+  inherit (lib.generators) mkLuaInline;
+  inherit (nvf.lib.nvim.dag) entryBefore entryAfter;
+in
 {
   vim = {
+    luaConfigRC = {
+      indent-blankline-pre =
+        entryBefore [ "indent-blankline" ] # lua
+          ''
+            -- A precursor Lua module that defines Lua locals that can be accessed in
+            -- ibl's setup function. All Lua locals defined here will be made available
+            -- to the DAG node in which ibl's setup is handled.
+            local ibl_highlight = {
+                "RainbowRed",
+                "RainbowYellow",
+                "RainbowBlue",
+                "RainbowOrange",
+                "RainbowGreen",
+                "RainbowViolet",
+                "RainbowCyan",
+            }
+          '';
+
+      indent-blankline-post =
+        entryAfter [ "indent-blankline" ] # lua
+          ''
+            -- This Lua chunk will be placed *after* ibl's setup is done.
+            local ibl_hooks = require "ibl.hooks"
+
+            -- create the highlight groups in the highlight setup hook, so they are reset
+            -- every time the colorscheme changes
+            ibl_hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+                vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+                vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+                vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+                vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+                vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+                vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+                vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+            end)
+
+            hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+          '';
+    };
+
     visuals = {
       nvim-cursorline = {
         enable = true;
@@ -22,6 +67,7 @@
           indent = {
             char = "│";
           };
+          scope.highlight = mkLuaInline "ibl_highlight";
         };
       };
 
@@ -31,6 +77,12 @@
 
       nvim-web-devicons.enable = true;
       fidget-nvim.enable = true;
+    };
+
+    # for rainbow-delimiters integration with indent-blankline
+    # vim.g.rainbow_delimiters option
+    globals = {
+      rainbow_delimiters = mkLuaInline "ibl_highlight";
     };
   };
 }
