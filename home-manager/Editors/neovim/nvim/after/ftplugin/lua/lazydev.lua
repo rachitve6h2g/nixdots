a74f33ea223305e0f2503facf48a52ec43702770
Rachit Kumar Verma <rachitverma1122@gmail.com>
2025-10-04 19:40:42 +0530

    update, nvim: lazydev (not working for lua)

--- home-manager/Editors/neovim/nvim/after/ftplugin/lua/lazydev.lua
+++ home-manager/Editors/neovim/nvim/after/ftplugin/lua/lazydev.lua
@@ -1,1 +1,15 @@
-require("lazydev").setup({})
+require("lazydev").setup({
+  enabled = function(root_dir)
+    return not vim.uv.fs_stat(root_dir .. "./.luarc.json")
+  end,
+  library = {
+    { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+  },
+  integrations = {
+    cmp = false,
+    coq = false,
+  },
+})
+vim.g.lazydev_enabled = true
+
+vim.lsp.enable("lua_ls")
