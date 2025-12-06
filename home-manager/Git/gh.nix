8ac9ba76f99bfde54ba3dbc81787ebb49fc6a39e
Rachit Kumar Verma <rachitverma1122@gmail.com>
2025-12-06 22:01:34 +0530

    add: gh hosts and settings

--- home-manager/Git/gh.nix
+++ home-manager/Git/gh.nix
@@ -7,6 +7,14 @@
         git_protocol = "ssh";
         editor = "nvim";
       };
+
+      hosts = {
+        "github.com" = {
+          git_protocol = "ssh";
+          users = "rachitve6h2g";
+          user = "rachitve6h2g";
+        };
+      };
     };
   };
 }
